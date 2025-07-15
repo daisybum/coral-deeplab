a2cf0940fbfad208a6fc9a2e903018d7e7d7709b
daisybum <hirvahapjh@gmail.com>
2025-07-15 13:16:28 +0900

    Chore: delete unused

--- compiler/compiler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-
-sudo apt-get update
-
-sudo apt-get install edgetpu-compiler
